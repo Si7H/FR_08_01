1d77c7d3030a45c3389f149b2a18b01b509f3b02
Arkadiusz Mecka <arkadiusz.mecka@gmail.com>
2018-03-13 00:39:00 +0100

    some work in main

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@ using namespace std;
 int main()
 {
 	string number;
-
+	cin >> number;
+	
+	for(unsigned i = 0; i < number.size(); ++i)
+	{
+		if(!(static_cast<int>(number[i] - 48) % 2))
+		{
+			cout << "TAK" << endl;
+			break;
+		}
+	}
+	
 }
