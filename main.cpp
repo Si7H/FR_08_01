2f985c67d8126bd922371b29cdca3525448c7d96
Arkadiusz Mecka <arkadiusz.mecka@gmail.com>
2018-03-13 00:47:40 +0100

    finished?

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 
 using namespace std;
 
+string DivideCheck(const string &inputNumber)
+{
+	for(unsigned i = 0; i < inputNumber.size(); ++i)
+	{
+		if(!(static_cast<int>(inputNumber[i] - 48) % 2))
+		{
+			return "TAK";
+		}
+	}
+	return "NIE";
+}
+
+
 int main()
 {
 	string number;
-	cin >> number;
-	
-	for(unsigned i = 0; i < number.size(); ++i)
-	{
-		if(!(static_cast<int>(number[i] - 48) % 2))
-		{
-			cout << "TAK" << endl;
-			break;
-		}
-	}
-	
+	cin >> number;	
+	cout << DivideCheck(number) << endl;
 }
